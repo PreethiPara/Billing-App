99840f598eb59a94f40f332f88e5d334f8ab8ca4
Para Preethi <92800650+PreethiPara@users.noreply.github.com>
2024-03-15 11:54:36 +0530

    Deleted files

--- src/Components/AddProduct/AddProduct.tsx
+++ src/Components/AddProduct/AddProduct.tsx
@@ -1,92 +1,15 @@
-import * as React from 'react';
-import { useRef } from 'react';
-import {Box, Button, Input, InputBase, TextField} from '@mui/material';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
-import { FormControl, IconButton, InputLabel, MenuItem} from '@mui/material';
-import "./AddProduct.scss";
-import DropDown from '../../Utils/DropDown/DropDown';
-import { sortList } from '../../Constants/sort';
-import BasicModal from '../ProductModal/BasicModal';
-import CustomForm from '../CustomForm/CustomForm';
-import { productFormList } from '../../Constants/branch';
-import { AddAPhoto } from '@mui/icons-material';
-import { theme } from '../../Constants/theme';
-import './AddProduct.scss'
-
-
-function AddPicture() {
-    const customInput=useRef<null | HTMLInputElement>(null);
-    
-    const [file,setFile]=React.useState<File>();
-    function getFile(event:React.BaseSyntheticEvent){
-        console.log(event.target.files[0])
-        setFile(event.target.files[0])
-    }
-    function handleClick(){
-        if(customInput.current!=null){
-            customInput.current.click()
-        }
-    }
-    
-    return(<>
-       {
-        file?(
-            <div className='after-upload'>
-                <BasicModal  
-                    buttonContent={"Close"} 
-                    element={
-                        <CustomForm  
-                            header="View Image" 
-                            listOfEntries={[]} 
-                            upperElement={<img style={{width:"50%"}} src={URL.createObjectURL(file)}/>} 
-                            lowerElement={<></>}/>
-                    }
-                    header="View Image"
-                />
-                <IconButton  className='reupload-picture-icon-button' style={{backgroundColor:theme.palette.secondary.light}} onClick={handleClick}>
-                {
-                    (<AddAPhoto className='add-picture-icon'/>)
-                }
-                </IconButton>
-            </div>
-        ):
-        (
-            <IconButton  className='add-picture-icon-button' style={{backgroundColor:theme.palette.secondary.light}} onClick={handleClick}>
-            {
-                (<AddAPhoto className='add-picture-icon'/>)
-            }
-            </IconButton>
-        )
-       }
-        
-       <p>{file?.name}</p>
-        <input className="disabled-input" type="file" id="avatar" name="avatar" accept="image/png, image/jpeg" onChange={getFile} ref={customInput}/>
-        </>
-    )
-}
+import React from "react";
+import { Box } from "@mui/material";
+import DropDown from "../../Utils/DropDown/DropDown";
+import {sortList } from '../../Constants/sort';
 function AddProduct(){
-   
-    return(<>
-        
-        <Box className="add-search-sort">
-                <BasicModal
-                    buttonContent={"Submit"}
-                    element={<CustomForm  header="Add Product" listOfEntries={productFormList} upperElement={<AddPicture />} lowerElement={<></>} />}
-                    header="Add product"
-                />
-                {/* <FormControl className='search-bar'>
-                    <FormLabel className="search-product-label">Search Product</FormLabel>
-                    <Autocomplete
-                    placeholder="Type to Search"
-                    options={topFilms}
-                    className='auto-complete'
-                    />
-                </FormControl> */}
-                {/* This is where you had JSX structure error */}
-                <DropDown listOfItems={sortList} Label='sort' />
-            </Box>
-        
-        </>
+    return(
+        <Box>
+            //add product modal
+            //search-functionality
+            <DropDown listOfItems={sortList} Label='Branch'/>
+        </Box>
     )
 }
 export default AddProduct;
+
--- src/Pages/ClientDashBoard/ClientDashBoard.tsx
+++ src/Pages/ClientDashBoard/ClientDashBoard.tsx
@@ -1,11 +1,13 @@
-import AddProduct from "../../Components/AddProduct/AddProduct";
+import React from "react";
+import AddProduct from "../../Components/AddProduct/AddProduct"
 import NavAppBar from "../../Components/NavAppBar/NavAppBar";
 
 function ClientDashBoard() {
-    return(<>
+    return(
+    <div>
         <NavAppBar/>
         <AddProduct/>
-    </>)
+    </div>)
 }
 
 export default ClientDashBoard;
